839c8498bb7ecdbaf7f323af8d1a3262e70e0b3c
seanbecker <seanbecker@SEANBECKERC9E6>
2023-07-10 16:22:20 -0500

    Add project files.

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml.Controls;
+
+namespace App1
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.RequiresPointer = RequiresPointer.Never;
+            this.Focus(Windows.UI.Xaml.FocusState.Programmatic);
+        }
+    }
+}
